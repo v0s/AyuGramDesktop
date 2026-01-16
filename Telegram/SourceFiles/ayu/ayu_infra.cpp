fc53285e408b6f01765664caa828ba813a5beb86
Vlad Roskov <vos@vos.uz>
2026-01-16 07:41:58 +0300

    Disable RCManager to avoid network requests

--- Telegram/SourceFiles/ayu/ayu_infra.cpp
+++ Telegram/SourceFiles/ayu/ayu_infra.cpp
@@ -46,7 +46,7 @@ void initWorker() {
 }
 
 void initRCManager() {
-	RCManager::getInstance().start();
+	// RCManager::getInstance().start();  // disabled to avoid unnecessary requests
 }
 
 void initTranslator() {
